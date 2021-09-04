979ec0887824aa948afa2f241256cd05d7d658ee
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-04 07:17:12 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -215,7 +215,6 @@ def start(update: Update, context: CallbackContext):
 
         else:
             update.effective_message.reply_text(
-                STICKERS,
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
